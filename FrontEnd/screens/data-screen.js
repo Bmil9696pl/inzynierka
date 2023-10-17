8b4b81ff9f5a6ef642071103b214ee722a6e6605
Bmil9696pl <84275559+Bmil9696pl@users.noreply.github.com>
2023-10-17 13:47:52 +0200

    added hide button to moving averages

--- FrontEnd/screens/data-screen.js
+++ FrontEnd/screens/data-screen.js
@@ -1,32 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
 import { VictoryCandlestick, VictoryLine, VictoryChart, VictoryTheme, VictoryZoomContainer } from "victory-native";
 import * as Progress from 'react-native-progress';
 import NumericInput from 'react-native-numeric-input'
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 const DataScreen = (data) => {
     const [numberOfDataPoints1, setNumberOfDataPoints1] = useState(5);
     const [numberOfDataPoints2, setNumberOfDataPoints2] = useState(10);
-
+    const [movAverage1Hidden, setMovingAverage1Hidden] = useState(true);
+    const [movAverage2Hidden, setMovingAverage2Hidden] = useState(true);
+    const [dataMA1, setDataMA1] = useState(null);
+    const [dataMA2, setDataMA2] = useState(null);
+    const firstUpdate1 = useRef(true);
+    const firstUpdate2 = useRef(true);
     const candleStickData = []
-    console.log("data from api")
-    
-    console.log(data["data"][2].goldValue)
-    for(let i = 0 ; i<96; i++){
+
+    useEffect(()=>{
+      if (firstUpdate1.current) {
+        firstUpdate1.current = false;
+        return;
+      }
+      setDataMA1(simpleMovingAverage(numberOfDataPoints1, candleStickData))
+    }, [numberOfDataPoints1])
+
+    useEffect(()=>{
+      if (firstUpdate2.current) {
+        firstUpdate2.current = false;
+        return;
+      }
+      setDataMA2(simpleMovingAverage(numberOfDataPoints2, candleStickData))
+    }, [numberOfDataPoints2])
+
+    function customSetMovingAverage1Hidden(){
+      setMovingAverage1Hidden(!movAverage1Hidden)
+      if(dataMA1 == null){
+        setDataMA1(simpleMovingAverage(numberOfDataPoints1, candleStickData))
+      }
+    }
+
+    function customSetMovingAverage2Hidden(){
+      setMovingAverage2Hidden(!movAverage2Hidden)
+      if(dataMA2 == null){
+        setDataMA2(simpleMovingAverage(numberOfDataPoints2, candleStickData))
+      }
+    }
+
+
+    for(let i = 0 ; i<data["data"].length; i+=4){
         var first = data["data"][i];
         var second= data["data"][i+1];  
         var third = data["data"][i+2];
+        var fourth = data["data"][i+2];
         const x = new Date(first.date)
         const open = first.goldValue
-        const close = third.goldValue
-        const high = Math.max(first.goldValue, second.goldValue, third.goldValue)
-        const low = Math.min(first.goldValue, second.goldValue, third.goldValue)
+        const close = fourth.goldValue
+        const high = Math.max(first.goldValue, second.goldValue, third.goldValue, fourth.goldValue)
+        const low = Math.min(first.goldValue, second.goldValue, third.goldValue, fourth.goldValue)
         let pom = {x, open, close, high, low}
         candleStickData.push(pom)
     }
     
-    //console.log(candleStickData)
+
 
     function generateRandomStockData() {
       const open = Math.random() * 100;
@@ -68,7 +104,7 @@ const DataScreen = (data) => {
         for(pastDataIterator = dataIterator; pastDataIterator >= 0; pastDataIterator--){
           if(numberOfIterations < nOfDataPoints){
             numberOfIterations++;
-            sum = data[pastDataIterator].close;
+            sum += data[pastDataIterator].close;
           }
         }
         returnValue.push({x: data[dataIterator].x ,y: sum/numberOfIterations});
@@ -76,15 +112,12 @@ const DataScreen = (data) => {
       return returnValue;
     }
   
-    dataMA1 = simpleMovingAverage(numberOfDataPoints1, candleStickData)
-    dataMA2 = simpleMovingAverage(numberOfDataPoints2, candleStickData)
-  
     let positiveSum = 0
     let sum = 0
     candleStickData.forEach(setRatio)
     let ratio = positiveSum/sum
     return (
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <View style={styles.tile}>
           <VictoryChart width={400} containerComponent={<VictoryZoomContainer zoomDomain={{x: [candleStickData[0].x, candleStickData[candleStickData.length - 1].x], y: [candleStickData[0].low, candleStickData[candleStickData.length - 1].high]}}/>} domainPadding={{ x: 0 }} theme={{
               axis: {
@@ -103,18 +136,19 @@ const DataScreen = (data) => {
               }
             }}>
           <VictoryCandlestick
+          candleRatio={1.2}
           style={{
             data: {
-              stroke: "#FFFFFF"
+              stroke: "#FFFFFF"            
             }
           }}
             candleColors={{
-            positive: "#34D17B", negative: "#EF4242" }}
+            positive: "#27D89B", negative: "#D82764" }}
             data={candleStickData}
           />
           <VictoryLine 
             style={{
-              data: { stroke: "#D1348A" },
+              data: { stroke: movAverage1Hidden ? "transparent" : "#FF9FC9" },
               parent: { border: "1px solid #ccc"}
             }}
             data={dataMA1}
@@ -122,7 +156,7 @@ const DataScreen = (data) => {
   
           <VictoryLine 
             style={{
-              data: { stroke: "#42EFEF" },
+              data: { stroke: movAverage2Hidden ? "transparent" : "#F7F5DD" },
               parent: { border: "1px solid #ccc"}
             }}
             data={dataMA2}
@@ -140,56 +174,70 @@ const DataScreen = (data) => {
           }}>
             <View style={styles.textContainer}>
             <Text style={{
-              color: "#34D17B",
-              fontSize: 27,
-              fontWeight: 400,
+              color: "#27D89B",
+              fontSize: RFPercentage(4),
             }}>
               {Math.round((ratio + Number.EPSILON) * 100) / 100}
             </Text>
             </View>
             <View style={styles.textContainer}>
             <Text style={{
-              color: "#EF4242",
-              fontSize: 27,
-              fontWeight: 400,
+              color: "#D82764",
+              fontSize: RFPercentage(4),
             }}>
               {Math.round(((1-ratio) + Number.EPSILON) * 100) / 100}
             </Text>
             </View>
           </View>
-          <Progress.Bar progress={ratio} width={320} height={30} color="#34D17B" unfilledColor="#EF4242"/>
+          <Progress.Bar progress={ratio} width={320} height={30} color="#27D89B" unfilledColor="#D82764"/>
         </View>
         <View style={styles.tile}>
-          <Text style={{color: '#D1348A', fontSize: 27, fontWeight: 400}}>
+          <Text style={{color: '#FF9FC9', fontSize: RFPercentage(4)}}>
             MOVING AVERAGE
           </Text>
-          <NumericInput 
-            value={numberOfDataPoints1}
-            onChange={setNumberOfDataPoints1}
-            minValue={1}
-            textColor='#FFFFFF' 
-            rightButtonBackgroundColor='#34D17B'
-            leftButtonBackgroundColor='#EF4242'
-            rounded='true'
-            borderColor='#3D3D3D'
-          />
+          <View style={{alignItems: "center", flexDirection:"row"}}>
+            <NumericInput
+              value={numberOfDataPoints1}
+              onChange={setNumberOfDataPoints1}
+              minValue={1}
+              textColor='#FFFFFF' 
+              rightButtonBackgroundColor='#27D89B'
+              leftButtonBackgroundColor='#D82764'
+              rounded='true'
+              borderColor='#3D3D3D'
+            />
+            <TouchableOpacity onPress={customSetMovingAverage1Hidden}>
+              <Image
+              style={styles.hidden}
+              source={require("../assets/img/3994371_eye_hidden_hide_invisible_private_icon.png")}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
         <View style={styles.tile}>
-          <Text style={{color: '#42EFEF', fontSize: 27, fontWeight: 400}}>
+          <Text style={{color: '#F7F5DD', fontSize: RFPercentage(4)}}>
             MOVING AVERAGE
           </Text>
-          <NumericInput 
-            value={numberOfDataPoints2}
-            onChange={setNumberOfDataPoints2}
-            minValue={1}
-            textColor='#FFFFFF' 
-            rightButtonBackgroundColor='#34D17B'
-            leftButtonBackgroundColor='#EF4242'
-            rounded='true'
-            borderColor='#3D3D3D'
-          />
+          <View style={{alignItems: "center", flexDirection:"row"}}>
+            <NumericInput 
+              value={numberOfDataPoints2}
+              onChange={setNumberOfDataPoints2}
+              minValue={1}
+              textColor='#FFFFFF' 
+              rightButtonBackgroundColor='#27D89B'
+              leftButtonBackgroundColor='#D82764'
+              rounded='true'
+              borderColor='#3D3D3D'
+            />
+            <TouchableOpacity onPress={customSetMovingAverage2Hidden}>
+              <Image
+                style={styles.hidden}
+                source={require("../assets/img/3994371_eye_hidden_hide_invisible_private_icon.png")}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
   
@@ -220,8 +268,13 @@ const DataScreen = (data) => {
     },
     text: {
       color: "#FFFFFF",
-      fontSize: 27,
-      fontWeight: 400
+      fontSize: RFPercentage(3.5),
+    },
+    hidden:{
+      resizeMode: "contain",
+      height: 30,
+      width: 30,
+      marginLeft: 50
     }
   });
 export default DataScreen;
--- FrontEnd/screens/home-screen.js
+++ FrontEnd/screens/home-screen.js
@@ -6,7 +6,7 @@ const HomeScreen = () => {
     const [dataFromApi, setData] = useState();
   
     const fetchData = async() =>{
-      const response = await fetch('http://192.168.124.145:8000/api/DailyData/')
+      const response = await fetch('http://192.168.124.186:8000/api/DailyData/')
       const data = await response.json()
       setData(data)
     }
